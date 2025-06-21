6bd266f323751f0f2c84b8d3bcb8eda3c0926a17
mirocle11 <47593784+mirocle11@users.noreply.github.com>
2025-06-21 16:23:12 +0800

    chore: fix hive adapter registration style

--- lib/data/local/hive_database.dart
+++ lib/data/local/hive_database.dart
@@ -32,39 +32,77 @@ class HiveDatabase {
   }
 
   static void registerAdapters() {
-    if (!Hive.isAdapterRegistered(0)) Hive.registerAdapter(LeagueAdapter());
-    if (!Hive.isAdapterRegistered(1)) Hive.registerAdapter(SeasonAdapter());
-    if (!Hive.isAdapterRegistered(2)) Hive.registerAdapter(TeamAdapter());
-    if (!Hive.isAdapterRegistered(3)) Hive.registerAdapter(PlayerAdapter());
-    if (!Hive.isAdapterRegistered(4)) Hive.registerAdapter(GameAdapter());
-    if (!Hive.isAdapterRegistered(5))
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(LeagueAdapter());
+    }
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(SeasonAdapter());
+    }
+    if (!Hive.isAdapterRegistered(2)) {
+      Hive.registerAdapter(TeamAdapter());
+    }
+    if (!Hive.isAdapterRegistered(3)) {
+      Hive.registerAdapter(PlayerAdapter());
+    }
+    if (!Hive.isAdapterRegistered(4)) {
+      Hive.registerAdapter(GameAdapter());
+    }
+    if (!Hive.isAdapterRegistered(5)) {
       Hive.registerAdapter(TransactionAdapter());
-    if (!Hive.isAdapterRegistered(6))
+    }
+    if (!Hive.isAdapterRegistered(6)) {
       Hive.registerAdapter(FinanceSheetAdapter());
-    if (!Hive.isAdapterRegistered(7)) Hive.registerAdapter(NewsItemAdapter());
-    if (!Hive.isAdapterRegistered(8)) Hive.registerAdapter(EventAdapter());
-    if (!Hive.isAdapterRegistered(9)) Hive.registerAdapter(BoxScoreAdapter());
-    if (!Hive.isAdapterRegistered(10)) Hive.registerAdapter(ContractAdapter());
-    if (!Hive.isAdapterRegistered(11))
+    }
+    if (!Hive.isAdapterRegistered(7)) {
+      Hive.registerAdapter(NewsItemAdapter());
+    }
+    if (!Hive.isAdapterRegistered(8)) {
+      Hive.registerAdapter(EventAdapter());
+    }
+    if (!Hive.isAdapterRegistered(9)) {
+      Hive.registerAdapter(BoxScoreAdapter());
+    }
+    if (!Hive.isAdapterRegistered(10)) {
+      Hive.registerAdapter(ContractAdapter());
+    }
+    if (!Hive.isAdapterRegistered(11)) {
       Hive.registerAdapter(ExpenseBreakdownAdapter());
-    if (!Hive.isAdapterRegistered(12))
+    }
+    if (!Hive.isAdapterRegistered(12)) {
       Hive.registerAdapter(IncomeBreakdownAdapter());
-    if (!Hive.isAdapterRegistered(13))
+    }
+    if (!Hive.isAdapterRegistered(13)) {
       Hive.registerAdapter(InjuryStatusAdapter());
-    if (!Hive.isAdapterRegistered(14))
+    }
+    if (!Hive.isAdapterRegistered(14)) {
       Hive.registerAdapter(LeagueRulesAdapter());
-    if (!Hive.isAdapterRegistered(15)) Hive.registerAdapter(RecordAdapter());
-    if (!Hive.isAdapterRegistered(16)) Hive.registerAdapter(SkillsAdapter());
-    if (!Hive.isAdapterRegistered(17))
+    }
+    if (!Hive.isAdapterRegistered(15)) {
+      Hive.registerAdapter(RecordAdapter());
+    }
+    if (!Hive.isAdapterRegistered(16)) {
+      Hive.registerAdapter(SkillsAdapter());
+    }
+    if (!Hive.isAdapterRegistered(17)) {
       Hive.registerAdapter(TeamColorsAdapter());
-    if (!Hive.isAdapterRegistered(18))
+    }
+    if (!Hive.isAdapterRegistered(18)) {
       Hive.registerAdapter(TeamStandingAdapter());
-    if (!Hive.isAdapterRegistered(19)) Hive.registerAdapter(EventTypeAdapter());
-    if (!Hive.isAdapterRegistered(20))
+    }
+    if (!Hive.isAdapterRegistered(19)) {
+      Hive.registerAdapter(EventTypeAdapter());
+    }
+    if (!Hive.isAdapterRegistered(20)) {
       Hive.registerAdapter(NewsCategoryAdapter());
-    if (!Hive.isAdapterRegistered(21)) Hive.registerAdapter(PositionAdapter());
-    if (!Hive.isAdapterRegistered(22))
+    }
+    if (!Hive.isAdapterRegistered(21)) {
+      Hive.registerAdapter(PositionAdapter());
+    }
+    if (!Hive.isAdapterRegistered(22)) {
       Hive.registerAdapter(TransactionTypeAdapter());
-    if (!Hive.isAdapterRegistered(30)) Hive.registerAdapter(GMProfileAdapter());
+    }
+    if (!Hive.isAdapterRegistered(30)) {
+      Hive.registerAdapter(GMProfileAdapter());
+    }
   }
 }
